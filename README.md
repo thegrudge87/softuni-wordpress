0dc60c621ebf48d9c661fe49e3f1ffe6b104af92
Dimitar Marinov <dimitar411locals@gmail.com>
2024-01-24 20:55:09 +0200

    initial commit; adding static templates for the custom theme; placeholder for the custom plugin; adding readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# SoftUni Games [Course Theme]
+
+Course Theme for the ["WordPress for Developers – ноември 2023"](https://softuni.bg/trainings/4388/wordpress-for-developers-november-2023)
+
+---
+
+### References
+* Source HTML Template - https://templatemo.com/tm-589-lugx-gaming 
---
+++ wp-content/plugins/softuni-games-plg/softuni-games-plg.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name:       SoftUni Games
+ * Description:       A custom Plugin for the "WordPress for Developers – ноември 2023" course
+ * Version:           0.1.0-beta
+ * Requires at least: 5.9
+ * Requires PHP:      7.3
+ * Author:            Dimitar Marinov
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       softuni-games-plg
+ * Domain Path:       /languages
+ */
