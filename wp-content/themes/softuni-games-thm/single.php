53b23ffa2c7ad188869794376db9c2b5670088f7
Dimitar Marinov <dimitar411locals@gmail.com>
2024-01-27 13:00:25 +0200

    enhancements in single.php

--- wp-content/themes/softuni-games-thm/single.php
+++ wp-content/themes/softuni-games-thm/single.php
@@ -1,11 +1,18 @@
+<?php
+/**
+ * @package softuni-games-thm
+ */
+
+?>
 <?php get_header(); ?>
 
     <div class="page-heading header-text">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1><?php wp_title('') ?></h1>
-                    <span class="breadcrumb"><a href="<?php echo get_home_url();?>"><?php _e('Home', SUT_Games::get_text_domain()); ?></a> <?php wp_title(); ?></span>
+                    <h1><?php the_title() ?></h1>
+                    <span class="breadcrumb"><a
+                                href="<?php echo get_home_url(); ?>"><?php _e( 'Home', SUT_Games::get_text_domain() ); ?></a> <?php wp_title(); ?></span>
                 </div>
             </div>
         </div>
@@ -13,19 +20,52 @@
 
     <main class="contact-page section">
 
-        <div class="container">
-            <div class="row">
+    <div class="container">
+        <div class="row">
+			<?php if ( have_posts() ) : ?>
 
-                <div class="col-12 text-center">
-                    <div class="section-heading ">
-                        <h6><?php wp_title('') ?></h6>
-                        <h2>Say Hello!</h2>
+				<?php while ( have_posts() ) : the_post(); ?>
+
+                    <div class="col-12 text-center">
+                        <div class="section-heading ">
+                            <h2><?php the_title(); ?></h2>
+                            <div>
+                                <span><?php echo get_the_author_posts_link() ?></span> |
+                                <span><?php echo get_the_date( 'F jS, Y', $post->ID ); ?></span>
+                            </div>
+                            <h6></h6>
+                        </div>
                     </div>
 
-	                <?php the_content(); ?>
-                </div>
-            </div>
+                    <div class="col-12">
+
+						<?php if ( has_post_thumbnail() ): ?>
+                            <div class="row align-items-center">
+                                <div class="col-md-5 col-lg-4">
+									<?php the_post_thumbnail( 'medium', array( 'alt' => get_the_title() ) ); ?>
+                                </div>
+                                <div class="col-md-7 col-md-8">
+									<?php the_content(); ?>
+                                </div>
+                            </div>
+						<?php else: ?>
+
+							<?php the_content(); ?>
+
+						<?php endif; ?>
+
+                    </div>
+
+				<?php endwhile; ?>
+
+
+			<?php else : ?>
+
+				<?php _e( 'Sorry, there is nothing I can show you.', SUT_Games::get_text_domain() ); ?>
+
+			<?php endif; ?>
         </div>
+    </div>
 
 
 <?php get_footer(); ?>
