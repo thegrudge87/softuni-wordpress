deb84bfe8cfe5de7c25ad4554eac266dea926dd5
Dimitar Marinov <dimitar411locals@gmail.com>
2024-02-04 14:03:58 +0200

    add guard check for the class; fix code styling; changes in the admin script enqueue rules

--- wp-content/plugins/softuni-games-plg/includes/class-sup-games.php
+++ wp-content/plugins/softuni-games-plg/includes/class-sup-games.php
@@ -6,16 +6,19 @@
  * @package softuni-games-plg
  */
 
-
-
 namespace SUP_Games;
 
 use SUP_Games\CPT_Games as CPT_Games;
+
+if ( ! class_exists( 'SUP_Games' ) ) :
+
 class SUP_Games {
 	private static $text_domain = 'softuni-games-plg';
 	private static $version = '0.1.0';
 
 	public function __construct() {
+
+		// Load and call all CTP & Metaboxes
 		$games = new CPT_Games();
 
 		add_action('admin_enqueue_scripts', array($this, 'enqueue_admin_assets'));
@@ -41,11 +44,19 @@ class SUP_Games {
 			if ( ! did_action( 'wp_enqueue_media' ) ) {
 				wp_enqueue_media();
 			}
+
 			// our custom JS
 			wp_enqueue_script(
 				'sup-script-js',
 				plugins_url( 'js/scripts.js', __DIR__ ),
-				array( 'jquery' )
+				array( 'jquery' ),
+				self::get_version(),
+				array(
+					'strategy' => 'defer',
+					'in_footer' => 'true',
+				)
 			);
 	}
 }
+
+endif;
