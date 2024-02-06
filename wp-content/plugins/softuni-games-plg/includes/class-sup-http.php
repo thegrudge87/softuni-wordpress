d2ffaf5582b1fd1aad9b312ff4ecb802fd6c23e9
Dimitar Marinov <dimitar411locals@gmail.com>
2024-02-06 23:06:50 +0200

    add comments on the SUP_Http class

--- wp-content/plugins/softuni-games-plg/includes/class-sup-http.php
+++ wp-content/plugins/softuni-games-plg/includes/class-sup-http.php
@@ -15,26 +15,43 @@ class SUP_Http {
 		add_action( 'init', array( $this, 'set_likes_cookie' ) );
 	}
 
+
+	/**
+	 * Set a cookie about which games were liked
+	 * by the user in the last 24 hours
+	 *
+	 * @param $data
+	 *
+	 * @return void
+	 */
 	public function set_likes_cookie( $data = null ) {
 		if ( ! isset( $_COOKIE['game_likes'] ) || $data ) {
-			setcookie( 'game_likes',  $data, strtotime( '+1 day' ), '/' );
+			setcookie( 'game_likes', $data, strtotime( '+1 day' ), '/' );
 		}
 	}
 
 	/**
+	 * Get the cookie and return the data as array
+	 *
 	 * @return array|false|string[]
 	 */
 	public static function get_likes_cookie_data() {
 		// Get the game_likes cookie, if it's not set return [];
 		if ( $_COOKIE['game_likes'] ) {
-			$raw_cookie = preg_replace('/[^0-9,]/', '', $_COOKIE['game_likes']);
+			$raw_cookie = preg_replace( '/[^0-9,]/', '', $_COOKIE['game_likes'] );
 
-			return explode( ',',  $raw_cookie);
+			return explode( ',', $raw_cookie );
 		}
 
 		return [];
 	}
 
+	/**
+	 * Handler that process the Ajax call
+	 * about increasing the likes for a game by post_id
+	 *
+	 * @return void
+	 */
 	public function game_likes_increase() {
 
 		// Guard Check that this will work only for the "add_game_like" action,
@@ -43,23 +60,23 @@ class SUP_Http {
 			return;
 		}
 
+		$meta_key = 'num_of_likes';
 		$post_id = esc_attr( $_POST['post_id'] );
+		$user_current_likes = $this->get_likes_cookie_data();
 
 		// Check do we have the post_id in the like_cookie array
-		$user_current_likes = $this->get_likes_cookie_data();
 		if ( in_array( $post_id, $user_current_likes ) ) {
 			wp_send_json( array(
 				"status" => "already liked",
 			) );
 		}
 
-		$meta_key = 'num_of_likes';
-
 		$likes_count = get_post_meta( $post_id, $meta_key, true );
 		if ( empty( $likes_count ) ) {
 			$likes_count = 0; // set a default value as 0 (zero);
 		}
-		$likes_count++;
+
+		$likes_count ++;
 		update_post_meta( $post_id, $meta_key, $likes_count );
 
 		$user_current_likes[] = $post_id;
@@ -67,7 +84,7 @@ class SUP_Http {
 
 		$response = array(
 			"status" => "liked",
-			"likes" => $likes_count
+			"likes"  => $likes_count
 		);
 
 		// Return JSON response to the Ajax call
