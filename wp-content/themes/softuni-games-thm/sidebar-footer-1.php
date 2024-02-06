cf4a1bf32cb1f381fe1d75c11f418a95f0c31e2e
Dimitar Marinov <dimitar411locals@gmail.com>
2024-02-06 19:29:10 +0200

    adding addtional check in the footer widget areas

--- wp-content/themes/softuni-games-thm/sidebar-footer-1.php
+++ wp-content/themes/softuni-games-thm/sidebar-footer-1.php
@@ -1,3 +1,5 @@
 <div id="sidebar-footer-1" class="sidebar col-sm-6 col-md-4 mb-3">
-	<?php dynamic_sidebar( 'footer-1' ); ?>
+	<?php if ( is_active_sidebar( 'footer-1' ) ): ?>
+		<?php dynamic_sidebar( 'footer-1' ); ?>
+	<?php endif; ?>
 </div>
--- wp-content/themes/softuni-games-thm/sidebar-footer-2.php
+++ wp-content/themes/softuni-games-thm/sidebar-footer-2.php
@@ -1,3 +1,5 @@
 <div id="sidebar-footer-1" class="sidebar col-sm-6 col-md-4 mb-3">
-	<?php dynamic_sidebar( 'footer-2' ); ?>
+	<?php if ( is_active_sidebar( 'footer-2' ) ): ?>
+		<?php dynamic_sidebar( 'footer-2' ); ?>
+	<?php endif; ?>
 </div>
